9293dded67f747a12a99330f48ecfa8f7f8467f0
SteelBook <shebang42@googlemail.com>
2021-02-20 18:17:15 +0100

    Retrieve shipping prices

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,7 +8,7 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
   items = this.cartService.getItems();
-  
+
   constructor(
     private cartService : CartService
   ) { }
